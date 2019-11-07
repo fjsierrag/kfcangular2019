1d9c68971f4dcb73b0e9c9a7ee318eb1b20aae85
Francisco Sierra <francisco.sierra@kfc.com.ec>
2019-11-06 20:30:20 -0500

    Primer Commit

--- projects/bookshop-website/src/app/app-routing.module.ts
+++ projects/bookshop-website/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import {HomeComponent} from "./home/home.component";
 
 
 const routes: Routes = [
-    {path: '', redirectTo: 'home'},
+    {path: '', redirectTo: '/home', pathMatch:'full'},
     {path: 'home', component: HomeComponent}
 ];
 
