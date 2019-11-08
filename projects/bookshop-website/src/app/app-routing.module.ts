546b8a6109846a3219bbe5193e84da37da12c192
Francisco Sierra <francisco.sierra@kfc.com.ec>
2019-11-08 18:21:43 -0500

    HomeComponent V2

--- projects/bookshop-website/src/app/app-routing.module.ts
+++ projects/bookshop-website/src/app/app-routing.module.ts
@@ -1,11 +1,7 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {HomeComponent} from "./home/home.component";
-
-
 const routes: Routes = [
-    {path: '', redirectTo: '/home', pathMatch:'full'},
-
+    { path: '', redirectTo:"home",pathMatch:"full" },
     { path: 'books', loadChildren: () => import('./books/books.module').then(m => m.BooksModule) }
 ];
 
