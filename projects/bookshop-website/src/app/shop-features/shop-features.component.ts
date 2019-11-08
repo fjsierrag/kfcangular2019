400dc2bc8410323b78a6e7b01a0109d38774ed5a
Francisco Sierra <francisco.sierra@kfc.com.ec>
2019-11-07 19:45:31 -0500

    ngFOR

--- projects/bookshop-website/src/app/shop-features/shop-features.component.ts
+++ projects/bookshop-website/src/app/shop-features/shop-features.component.ts
@@ -1,16 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {faCreditCard, faShieldAlt, faTags, faTruckMoving} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
-  selector: 'app-shop-features',
-  templateUrl: './shop-features.component.html',
-  styleUrls: ['./shop-features.component.scss']
+    selector: 'app-shop-features',
+    templateUrl: './shop-features.component.html',
+    styleUrls: ['./shop-features.component.scss']
 })
 export class ShopFeaturesComponent implements OnInit {
+    features = [
+        {
+            icon: faTruckMoving,
+            name: "quick delivery",
+            text: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit."
+        },
+        {icon: faCreditCard, name: "pay with easy", text: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit."},
+        {icon: faTags, name: "best deal", text: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit."},
+        {icon: faShieldAlt, name: "secured payment", text: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit."},
+    ];
 
+    constructor() {
+    }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
