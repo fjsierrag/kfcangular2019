dc476107bd5174c6cabad22345190d71325bb53b
Francisco Sierra <francisco.sierra@kfc.com.ec>
2019-11-07 20:29:58 -0500

    HomeComponent V2

--- projects/bookshop-website/src/app/app.module.ts
+++ projects/bookshop-website/src/app/app.module.ts
@@ -1,38 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome'
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './global/header/header.component';
-import { LogoComponent } from './global/logo/logo.component';
-import { SearchComponent } from './global/search/search.component';
-import { UtilNavComponent } from './global/util-nav/util-nav.component';
-import { MenuComponent } from './global/menu/menu.component';
-import { FooterComponent } from './global/footer/footer.component';
-import { HomeComponent } from './home/home.component';
-import { HeroComponent } from './home/hero/hero.component';
-import { ShopFeaturesComponent } from './home/shop-features/shop-features.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './global/header/header.component';
+import {LogoComponent} from './global/logo/logo.component';
+import {SearchComponent} from './global/search/search.component';
+import {UtilNavComponent} from './global/util-nav/util-nav.component';
+import {MenuComponent} from './global/menu/menu.component';
+import {FooterComponent} from './global/footer/footer.component';
+import {HomeModule} from "./home/home.module";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    LogoComponent,
-    SearchComponent,
-    UtilNavComponent,
-    MenuComponent,
-    FooterComponent,
-    HomeComponent,
-    HeroComponent,
-    ShopFeaturesComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FontAwesomeModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        LogoComponent,
+        SearchComponent,
+        UtilNavComponent,
+        MenuComponent,
+        FooterComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FontAwesomeModule,
+        HomeModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- projects/bookshop-website/src/app/home/home.module.ts
+++ projects/bookshop-website/src/app/home/home.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-
+import {HeroComponent} from "./hero/hero.component";
+import {ShopFeaturesComponent} from "./shop-features/shop-features.component";
 
 @NgModule({
-    declarations: [HomeComponent],
+    declarations: [
+        HomeComponent,
+        HeroComponent,
+        ShopFeaturesComponent],
     imports: [
         CommonModule,
+        FontAwesomeModule,
         HomeRoutingModule
     ]
 })
