fc349723fc962d859fe7461296665f5477b1f08f
Francisco Sierra <francisco.sierra@kfc.com.ec>
2019-11-11 20:36:15 -0500

    Formulario de registro - V0.1

--- projects/bookshop-website/src/app/security/security.module.ts
+++ projects/bookshop-website/src/app/security/security.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { SecurityRoutingModule } from './security-routing.module';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import {FormsModule} from "@angular/forms";
 
 
 @NgModule({
   declarations: [RegisterComponent, LoginComponent],
   imports: [
     CommonModule,
-    SecurityRoutingModule
+    SecurityRoutingModule,
+    FormsModule,
   ]
 })
 export class SecurityModule { }
